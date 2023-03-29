47771caa3091c3e7bdbcc215094e8afa13796ff3
Bea-ro <b_a_ro@hotmail.com>
2023-03-29 20:37:20 +0200

    revise last images routes

--- src/components/techs-lists.js
+++ src/components/techs-lists.js
@@ -12,16 +12,16 @@ ${PERSONALDATA.techs.map((tech) => `<li class="tech">
 export const experienceTechsList = `
 <ul class="experience-techs">
 <li class="experience-tech">
-<img src='../../public/icons/wp-icon.png' alt="Wordpress Icon" class="tech-icon"/>
+<img src='/icons/wp-icon.png' alt="Wordpress Icon" class="tech-icon"/>
 </li>
 <li class="experience-tech">
-<img src='../../public/icons/canva-icon.png' alt="Canva Icon" class="tech-icon" id="canva-icon"/>
+<img src='/icons/canva-icon.png' alt="Canva Icon" class="tech-icon" id="canva-icon"/>
 </li>
 <li class="experience-tech" id="sap-icon">
-<img src='../../public/icons/sap-icon.png' alt="SAP Icon" class="tech-icon"/>
+<img src='/icons/sap-icon.png' alt="SAP Icon" class="tech-icon"/>
 </li>
 <li class="experience-tech">
-<img src='../../public/icons/excel-icon.png' alt="Excel Icon" class="tech-icon"/>
+<img src='/icons/excel-icon.png' alt="Excel Icon" class="tech-icon"/>
 </li>
 </ul> 
 `;
