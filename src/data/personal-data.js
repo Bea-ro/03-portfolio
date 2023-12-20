fc20744b46ea4f78489113d330c6e529b7a475b1
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2023-12-20 14:30:01 +0100

    add icon to HTMLindex head

--- src/data/personal-data.js
+++ src/data/personal-data.js
@@ -1,5 +1,6 @@
 export const PERSONALDATA = {
-  image: '/images/personal-img.jpg',
+  // image: '/images/personal-img.jpg',
+  image: 'https://res.cloudinary.com/dnlceaase/image/upload/v1701274136/personal-img_oeu9vf.jpg',
   name: 'Beatriz',
   surname: 'Rodríguez',
   city: 'Madrid',
@@ -17,24 +18,32 @@ export const PERSONALDATA = {
     { icon: '/icons/github-icon.png', name: 'Github' },
     { icon: '/icons/nodejs-icon.png', name: 'Node' },
     { icon: '/icons/express-icon.png', name: 'Express' },
-    { icon: '/icons/mongodb-icon.png', name: 'MongoDB' },
+    { icon: '/icons/mongodb-icon.png', name: 'MongoDB' }
   ],
   description: 'With the encourage of a beginner and wide experience in Marketing',
   profiles: [
     { link: 'https://github.com/Bea-ro', icon: '/icons/github-icon.png', name: 'GitHub' },
-    { link: 'https://www.linkedin.com/in/beatrizrodriguezmaya/', icon: '/icons/linkedin-icon.png', name: 'LinkedIn' },
+    {
+      link: 'https://www.linkedin.com/in/beatrizrodriguezmaya/',
+      icon: '/icons/linkedin-icon.png',
+      name: 'LinkedIn'
+    }
   ],
-  reasons: ['I was lucky to be trained by the best',
+  reasons: [
+    'I was lucky to be trained by the best',
     'I have the encourage and enthusiam of a beginner ',
-    'I am focus on best practices and I am not afraid of refactoring'],
-  insights: ['I just simply enjoy coding',
+    'I am focus on best practices and I am not afraid of refactoring'
+  ],
+  insights: [
+    'I just simply enjoy coding',
     'I love challenges and give my very best',
-    'I see myself as a restless mind'],
+    'I see myself as a restless mind'
+  ],
   likes: [
     { icon: '/icons/nature-icon.jpg', name: 'nature' },
     { icon: '/icons/dancing-icon.jpg', name: 'dancing' },
     { icon: '/icons/morning-icon.png', name: 'morning' },
     { icon: '/icons/sport-icon.jpg', name: 'sport' },
-    { icon: '/icons/breaking-bad-icon.jpg', name: 'breaking-bad' },
-  ],
+    { icon: '/icons/breaking-bad-icon.jpg', name: 'breaking-bad' }
+  ]
 };
